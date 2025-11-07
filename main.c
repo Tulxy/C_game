299fa49bedf6826088d65c50fc1b91852220eab1
Tulxy <tomas.vylimec@gmail.com>
2025-11-07 10:07:46 +0100

    commit arena

--- main.c
+++ main.c
@@ -1,36 +1,39 @@
 #include <stdio.h>
 #include <unistd.h>
 
-// Maximální počet životů hráče
-unsigned short max_hp = 10; // 10HP je maximum
-// Aktuální počet životů hráče
-unsigned short hp = 5; // 10HP v základu
-
-// Množství peněz hráče
+// Proměnné
+unsigned short max_hp = 100; // 10HP je maximum
+unsigned short hp = 100; // 10HP v Klaudiusz
 unsigned int money = 10; // 10 zlatých v základu
-// Zkušenostní body hráče
-int xp = 0;
+int xp = 0; // Zkušenostní body hráče
+
 
 // Funkce pro výpis aktuálních statistik hráče
 void print_stats() {
-    int empty_hp = max_hp - hp; // počet chybějících životů
-    int level = xp / 10; // výpočet úrovně na základě XP
+    int level = xp / 10;
+    int full_hearts = hp / 10;         // Počet plných srdíček
+    int empty_hearts = (max_hp - hp) / 10; // Počet prázdných srdíček
 
     printf("\n");
-    printf("Level: %d 🔮", level);
-    printf("\n");
+    printf("Level: %d 🔮\n", level);
     printf("HP: ");
+
     // Vykreslení plných životů
-    for (int i = 0; i < hp; i++) {
+    for (int i = 0; i < full_hearts; i++) {
         printf("❤️");
     }
+
     // Vykreslení prázdných životů
-    for (int i = 0; i < empty_hp; i++) {
+    for (int i = 0; i < empty_hearts; i++) {
         printf("🩶");
     }
-    printf("\n");
+
+    printf(" (%d/%d)\n", hp, max_hp); // zobrazí číselně HP
     printf("Money: %d 💵\n", money);
 }
+
+
+// Pub functions
 
 // Výpis nabídky hospody
 void pub_print() {
@@ -61,7 +64,6 @@ void stats_add(int add_hp, int pay_money) {
     money -= pay_money;
 }
 
-
 // Obsluha menu hospody, aktualizuje HP a peníze podle volby
 int pub_menu() {
     pub_print();
@@ -73,20 +75,20 @@ int pub_menu() {
 
     switch (choice) {
         case 1:
-            printf("That was refreshing! [+2 HP]\n");
-            stats_add(2, 3);
+            printf("That was refreshing! [+20 HP]\n");
+            stats_add(20, 3);
             break;
         case 2:
-            printf("Popici plznička! [+1 HP]\n");
-            stats_add(1, 1);
+            printf("Popici plznička! [+10 HP]\n");
+            stats_add(10, 1);
             break;
         case 3:
-            printf("Amazing! [+4 HP]\n");
-            stats_add(4, 5);
+            printf("Amazing! [+40 HP]\n");
+            stats_add(40, 5);
             break;
         case 4:
-            printf("Amazing chiken! [+10 HP]\n");
-            stats_add(10, 8);
+            printf("Amazing chiken! [+80 HP]\n");
+            stats_add(80, 8);
             break;
         case 5:
             printf("Leaving th pub!\n");
@@ -98,8 +100,11 @@ int pub_menu() {
     return choice;
 }
 
+
+// Training Camp functions
+
 // Odpočet času tréninku s vizuálním zobrazením a přidáním XP
-int count_down(int time, int add_xp) {
+void count_down(int time, int add_xp) {
     printf("Training ends in: ");
     for (int i = time; i > 0; i--) {
         int min = i / 60;
@@ -190,6 +195,87 @@ void training_menu() {
     }
 }
 
+
+// Arena functions
+
+void attacks(int monster_hp, int monster_attack, char monster_name[]) {
+    int attack_turn = 1;
+    printf("You're fighting with %s!\n", monster_name);
+
+    while (hp > 0 && monster_hp > 0) {
+        if (attack_turn % 2 == 1) {
+            printf("Your turn, Attack!\n");
+            monster_hp -= 1;
+            printf("Monster HP: %d\n\n", monster_hp);
+        } else {
+            printf("The %s is fighting back!\n", monster_name);
+            hp -= monster_attack;
+            print_stats();
+        }
+        attack_turn++;
+    }
+
+    if (hp <= 0) {
+        printf("You have been defeated!\n");
+    } else {
+        printf("You defeated the Slime!\n");
+        money += 1;
+    }
+
+    attack_turn = 1;
+}
+
+void arena_menu() {
+    int level = xp / 10; // výpočet úrovně na základě XP
+
+    printf("-- Who do you want to fight with? --\n");
+    printf("1. Slime ...... 10HP [+1 Money, -1 HP]\n");
+    printf("2. Skeleton ... 25HP [+3 Money, -3 HP]\n");
+    printf("3. NEGR ....... 50HP [+10 Money, -8 HP]\n");
+    printf("4. Martin ..... 150HP [+20 Money, -5 HP]\n");
+    if (level >= 10) {
+        printf("5. Hangárová Držka ..... 300HP [+50 Money, -20 HP]\n");
+    } else {
+        printf("5. -- You need level 10 to unlock! --\n");
+    }
+    printf("6. Back\n");
+
+    printf("Your choice: ");
+    int choice;
+    scanf("%d", &choice);
+    printf("\n");
+
+    switch (choice) {
+        case 1:
+            attacks(10, 3, "Slime");
+            break;
+        case 2:
+            attacks(25, 5, "Skeleton");
+            break;
+        case 3:
+            attacks(50, 15, "NEGR");
+            break;
+        case 4:
+            attacks(150, 10, "Martin");
+            break;
+        case 5:
+            if (level >= 10) {
+                attacks(300, 20, "Hangárová Držka");
+            } else {
+                printf("5. -- You need level 10 to unlock! --\n");
+            }
+            break;
+        case 6:
+            printf("You're leaving, too scared huh?");
+            break;
+        default:
+            printf("Invalid choice!");
+    }
+}
+
+
+// Game
+
 // Prototyp funkce hry pro rekurzivní volání
 void game();
 
@@ -245,6 +331,7 @@ void game(void) {
             break;
         case 3:
             printf("Ypu chose to go to Arena");
+            arena_menu();
             break;
         case 4:
             printf("Thanks for playing!");
